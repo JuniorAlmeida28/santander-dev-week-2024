aa72b6aa77fbbcdca606d2debcd5d27051f7062e
Junior Almeida <jrjunioralmeida@outlook.com>
2024-06-17 13:52:54 -0300

    Hotfix "precision" e "scale".

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
 
     private String agency;
 
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13,scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "additional_Limit", scale = 13, precision = 2)
+    @Column(name = "additional_Limit", precision = 13,scale = 2)
     private BigDecimal  limit;
 
     public Long getId() {
